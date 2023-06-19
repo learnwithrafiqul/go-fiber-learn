79a19f936c2f9c3411def0a7333def6925387471
rafiqul-altersense <110759994+rafiqul-altersense@users.noreply.github.com>
2023-06-20 05:55:27 +0600

    d

--- server.go
+++ server.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 )
 
-func main() {
-    app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
+var getMyName = func(c *fiber.Ctx) error {
+    return c.SendString("Hello, World! I am MD Rafiqul Islam")
+}
 
-        return c.SendString("Hello, World!")
+func main(){
+    app:= fiber.New()
+
+    //  Routes
+    app.Get("/",getMyName).Name("Home Page")
+    
+    app.Get("/loop/:times",func(c *fiber.Ctx) error{
+        str_to_int,_ := strconv.Atoi(c.Params("times")) 
+        for i :=1; i<= str_to_int; i++{
+            fmt.Println(i)
+        }
+        return c.SendString("Loop is running "+c.Params("times"))
     })
 
-    app.Get("/api", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
 
-    app.Listen(":3000") 
+
+    a := app.Group("/api")
+    a.Name("API")
+    a.Get("/hello",getMyName).Name("Api Hello")
+
+
+
+    app.Get("/metrics", monitor.New(monitor.Config{Title: "MyService Metrics Page"}))
+
+
+
+    //  Start Server 
+    fmt.Println("Server running at http://localhost:3000")
+    data, _ := json.MarshalIndent(app.Stack(), "", "  ")
+    fmt.Println(string(data))
+    app.Listen(":3000")
 }
